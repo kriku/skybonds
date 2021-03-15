bb9b73a320bb1c7a9a4809492884914cb14bb8ff
Gosha Krikun <kpuk@yandex-team.ru>
2021-03-15 05:23:15 +0300

    chore: refine benchmark shell script

--- 1/benchmark.sh
+++ 1/benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-node ./build/index.load.js 1 60 > 1child.csv
-node ./build/index.load.js 2 30 > 2child.csv
-node ./build/index.load.js 3 20 > 3child.csv
-node ./build/index.load.js 4 15 > 4child.csv
-node ./build/index.load.js 5 12 > 5child.csv
+time node ./build/index.load.js 1 1 | tee data/s2c1x1.csv
+time node ./build/index.load.js 1 2 | tee data/s2c1x2.csv
+time node ./build/index.load.js 1 10 | tee data/s2c1x10.csv
+
+time node ./build/index.load.js 1 1 | tee data/s2c1.csv
+time node ./build/index.load.js 2 1 | tee data/s2c2.csv
+time node ./build/index.load.js 3 1 | tee data/s2c3.csv
+time node ./build/index.load.js 4 1 | tee data/s2c4.csv
+time node ./build/index.load.js 10 1 | tee data/s2c10.csv
